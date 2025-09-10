c2ad5c76b18210794083eb32ea21f1bc47cd134a
Muhammad Ibrahim <Sweetbutt78634@gmail.com>
2025-09-10 23:35:09 +0000

    remove thisd

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -41,12 +41,6 @@ export default function LoginForm() {
             Login
             </Button>
         </div>
-        <div className="mt-4 text-center text-sm">
-            Don&apos;t have an account?{" "}
-            <Link href="#" className="underline">
-            Sign up
-            </Link>
-        </div>
     </div>
   )
 }
