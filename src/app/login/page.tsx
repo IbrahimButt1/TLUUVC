d2b7193ccca3b7e4f9ba64b05f07e9490c315543
Muhammad Ibrahim <Sweetbutt78634@gmail.com>
2025-09-10 23:37:05 +0000

    only for administrator login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -35,10 +35,10 @@ export default function LoginPage() {
                 <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
                     <div className="flex flex-col space-y-2 text-center">
                         <h1 className="text-2xl font-semibold tracking-tight">
-                        Login to your account
+                        Administrator Login
                         </h1>
                         <p className="text-sm text-muted-foreground">
-                        Enter your email below to login to your account
+                        Enter your credentials to access the admin dashboard.
                         </p>
                     </div>
                     <LoginForm />
